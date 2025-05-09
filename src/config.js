d42ce86da45cdd75431a08203f62a23dca560968
Leninkumar vadivel <apple@Apples-MacBook-Pro.local>
2025-05-09 21:29:08 +0530

    six

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@
 const hostname = window.location.hostname;
 
 // If running locally, use localhost, otherwise use the current hostname
-export const API_URL = http://13.51.6.112:5000/
+export const API_URL = "http://13.51.6.112:5000";
